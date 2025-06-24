dd5b1671f77743db049c50f93c29081a89b63299
yuexiangjun <863821552@qq.com>
2025-06-24 17:30:26 +0800

    修改

---
+++ src/main/java/com/lucky/infrastructure/repository/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package com.lucky.infrastructure.repository.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("redis"); // 使用服务名
+        config.setPort(6379);
+        config.setPassword("fx1234@");
+        return new LettuceConnectionFactory(config);
+    }
+}
