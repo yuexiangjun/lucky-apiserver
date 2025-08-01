113918558a010ab881020b9a0ed13fedaf07fff6
yuexiangjun <863821552@qq.com>
2025-08-01 15:12:04 +0800

    a

--- src/main/java/com/lucky/application/interceptor/JwtUtils.java
+++ src/main/java/com/lucky/application/interceptor/JwtUtils.java
@@ -1,12 +1,12 @@
 package com.lucky.application.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.lucky.domain.exception.BusinessException;
+import io.jsonwebtoken.*;
 import org.apache.logging.log4j.util.Strings;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -44,6 +44,8 @@ public class JwtUtils {
 	 * @return 数据声明
 	 */
 	public static Claims parseToken(String token) {
+		checkToken(token);
+
 		return Jwts.parser()
 				.setSigningKey(secret)
 				.parseClaimsJws(token)
@@ -79,6 +81,7 @@ public class JwtUtils {
 	 * @return 用户ID
 	 */
 	public static String getUserId(String token) {
+
 		Claims claims = parseToken(token);
 		return getValue(claims, SecurityConstants.DETAILS_USER_ID);
 	}
@@ -134,5 +137,41 @@ public class JwtUtils {
 			return (String) value;
 		}
 		return value.toString();
+	}
+
+
+	private static void checkToken(String token) {
+		if (Objects.isNull(token)) {
+			throw BusinessException.newInstance("令牌不能为空");
+
+		}
+		Claims claims = null;
+		try {
+			claims = JwtUtils.parseToken(token);
+		} catch (ExpiredJwtException e) {
+
+			throw BusinessException.newInstance("token过期");
+
+		} catch (MalformedJwtException e) {
+
+			throw BusinessException.newInstance("json web token格式错误");
+
+		} catch (Exception e) {
+
+			throw BusinessException.newInstance("token解析异常");
+		}
+		if (claims == null) {
+			throw BusinessException.newInstance("令牌已过期或验证不正确");
+		}
+		String userId = JwtUtils.getUserId(claims);
+		String username = JwtUtils.getUserName(claims);
+		Integer client = JwtUtils.getClient(claims);
+
+		if (Strings.isBlank(userId) ||
+				Strings.isBlank(username) ||
+				Objects.isNull(client) || !List.of(1, 2).contains(client)
+		) {
+			throw BusinessException.newInstance("令牌验证失败");
+		}
 	}
 }
